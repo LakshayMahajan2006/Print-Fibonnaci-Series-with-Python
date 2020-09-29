aaa413e78bf9ce9e94201fc56bbad5307292ca2f
LakshayMahajan2006 <69849425+LakshayMahajan2006@users.noreply.github.com>
2020-09-29 14:16:09 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,11 @@
+# Print-Fibonnaci-Series-with-Python
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a 
+        a, b = b, a+b
+
+for f in fib():
+    if f > 100:
+        break
+    print(f)
